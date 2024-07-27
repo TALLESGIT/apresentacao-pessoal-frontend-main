cda83d067f805c05d9023efbc673f63595445fd7
Talles Coelho <talescoelho43@gmail.com>
2024-07-27 10:55:00 -0300

    "Updated index.html to add class 'bg-slate-600' ao body, adicionei dependências react-router-dom e @biomejs/biome ao package.json, criei um novo componente About e atualizei o arquivo yarn.lock."

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import About from "./components/About";
+
 function App() {
 	return (
-		<div bg-blue-600>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		<div>
+			<About />
 		</div>
 	);
 }
---
+++ src/components/About/index.jsx
@@ -0,0 +1,29 @@
+function About() {
+	return (
+		<section>
+			<img src="" alt="" />
+			<div>
+				<h2 className="text-3xl font-bold mb-4 text-center">Sobre Mim</h2>
+				<p className="text-lg">
+					🧑‍💻 Desenvolvedor Fron-end | Apaixonado por Transformar Códigos em
+					Experiências Inovadoras. Desde que construí meu primeiro computador,
+					aos 16 anos, sabia que a tecnologia era minha vocação. A sensação de
+					realização ao resolver problemas complexos e criar soluções inovadoras
+					tem me motivado desde então… diligente com experiência em projetos
+					freelance e especialização em Javascript, React, CSS e Node.
+					Comprometido com a criação de interfaces de usuário intuitivas e
+					performáticas. Atualmente aprimorando conhecimentos técnicos através
+					do curso superior de Análise e Desenvolvimento de Sistemas, o que
+					reforça uma base sólida para solucionar desafios complexos de
+					programação e colaborar efetivamente em ambientes de desenvolvimento
+					ágil..
+				</p>
+			</div>
+			<div>
+				<div>Tales Coelho</div>
+			</div>
+		</section>
+	);
+}
+
+export default About;
